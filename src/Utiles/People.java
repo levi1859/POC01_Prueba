38d2f46f8aab90fa57748d93f01189e2c7054538
Levi Barraza <Levi Barraza@DESKTOP-CTMP6SQ>
2019-09-07 00:35:51 -0400

    Añadir Clase y achivo de Entidades Bancarias

---
+++ src/Utiles/People.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utiles;
+
+/**
+ *
+ * @author Levi Barraza
+ */
+public class People {
+    String name;
+    
+    
+}
